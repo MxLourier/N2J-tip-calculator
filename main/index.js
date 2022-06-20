baa4d14bdae31e5854723aabed12f92e11ade3aa
Maya Lourier <maya.v.lourie@gmail.com>
2022-06-20 14:07:26 +0300

    Currently not working: reset button should be disabled when Bill and NoP fileds are both 0

--- main/index.js
+++ main/index.js
@@ -2,17 +2,42 @@ let inputNumber = document.getElementById("number-input");
 let notZero = document.getElementById("not-zero");
 let custom = document.getElementById("custom");
 let button15 = document.getElementById("button15");
+let reset = document.getElementById("reset");
 
-function focusOut(event){
+
+
+var billZero = true; //Variables to check if the corresponding fields have a
+var peopleZero = true; //value of 0.
+
+function makeButtonBlue(){ //15% button is preselected by default and is
+  //highlighted with cyan. When you select a different tip % the 15% button
+  //becomes dark blue like the rest of the unselected buttons.
+  button15.className = " button-blue";
+}
+
+function focusOutBill(event){
+  if(event.target.value==0){
+    billZero = true;
+  }else{
+    billZero = false;
+  }
+  resetControl();
+}
+
+function focusOut(event){//Shows a warning when a value of 0 has been entered
+  //in the NoP field
   if(event.target.value==0){
     inputNumber.style.outlineStyle="solid";
     inputNumber.style.outlineColor = "hsl(13, 70%, 61%)";
     inputNumber.style.outlineWidth = "2px";
     notZero.style.display = "inline";
+    peopleZero = true;
   } else {
     inputNumber.style.outlineStyle="none";
     notZero.style.display = "none";
+    peopleZero = false;
   }
+  resetControl();
 }
 
 function focusIn(){
@@ -42,3 +67,10 @@ function customFocusOut(event) {
 function focusOut15(){
  button15.className="button-blue";
 }
+
+function resetControl(){
+  if(billZero && peopleZero){
+    reset.style.backgroundColor = "red";
+    reset.disabled = "disabled";
+  }
+} // This function disables "Reset" button when both Bill and NoP fields have a 0 input.
