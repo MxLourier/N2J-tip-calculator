771ea02c2059a0660eba3f8c622f1b9705accddd
Maya Lourier <maya.v.lourie@gmail.com>
2022-06-20 12:10:54 +0300

    fixed issue with NoP field, configured button 15%, fixed alignment for custom button

--- main/index.js
+++ main/index.js
@@ -1,5 +1,7 @@
 let inputNumber = document.getElementById("number-input");
 let notZero = document.getElementById("not-zero");
+let custom = document.getElementById("custom");
+let button15 = document.getElementById("button15");
 
 function focusOut(event){
   if(event.target.value==0){
@@ -11,4 +13,32 @@ function focusOut(event){
     inputNumber.style.outlineStyle="none";
     notZero.style.display = "none";
   }
+}
+
+function focusIn(){
+  inputNumber.style.outlineStyle="solid";
+  inputNumber.style.outlineColor = "hsl(172, 67%, 45%)";
+  inputNumber.style.outlineWidth = "2px";
+  notZero.style.display = "none";
+}
+
+function customFocus() {
+  custom.style.textAlign = "right";
+  custom.style.padding = "0 5% 0 0";
+  custom.placeholder = '';
+}
+
+function customFocusOut(event) {
+  if(!event.target.value){
+    custom.style.textAlign = "center";
+    custom.style.padding = "0 2.5% 0 2.5%";
+    custom.placeholder = 'Custom';
+  }else {
+    custom.style.textAlign = "right";
+    custom.style.padding = "0 5% 0 0";
+  }
+}
+
+function focusOut15(){
+ button15.className="button-blue";
 }
